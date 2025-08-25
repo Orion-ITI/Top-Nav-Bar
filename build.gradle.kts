81d94b2149e3589577f4f9c441c597469b622140
Ali-Elmansoury <ali.elmansoury21@gmail.com>
2025-08-26 01:50:08 +0300

    Fixed settings presence, size of device list item

--- build.gradle.kts
+++ build.gradle.kts
@@ -71,7 +71,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.github.Ali-Elmansoury"
             artifactId = "orion-navigation-bars"
-            version = "1.0.5"
+            version = "1.0.6"
             afterEvaluate {
                 from(components["release"])
             }
